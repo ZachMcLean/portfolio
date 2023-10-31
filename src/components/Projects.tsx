7a597124b97704af4d4d9db120ebe58b8af17a5b
Zach McLean <zmclean11@gmail.com>
2023-10-31 14:50:16 -0500

    updated animations for project cards | on hover

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -23,8 +23,8 @@ type ProjectProps = (typeof projectsData)[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
 	return (
-		<section className="sm:h-[20rem] relative bg-gray-100 max-w-[42rem] border border-black-5 overflow-hidden sm:pr-8 mb-3 sm:mb-8 last:mb-0">
-			<div className="h-full flex flex-col pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%]">
+		<section className="group sm:h-[20rem] relative bg-gray-100 max-w-[42rem] border border-black-5 overflow-hidden sm:pr-8 mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition">
+			<div className="h-full flex flex-col pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] group-even:ml-[20rem]">
 				<h3 className="text-2xl font-semibold">{title}</h3>
 				<p className="mt-2 leading-relaxed text-gray-700">{description}</p>
 				<ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
@@ -42,7 +42,20 @@ function Project({ title, description, tags, imageUrl }: ProjectProps) {
 				src={imageUrl}
 				alt="Project I created"
 				quality={95}
-				className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl"
+				className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
+				
+				group-hover:scale-[1.04] 
+				group-hover:-translate-x-3 
+				group-hover:translate-y-3 
+				group-hover:-rotate-2 
+
+				group-even:group-hover:translate-x-3 
+				group-even:group-hover:translate-y-3 
+				group-even:group-hover:rotate-2 
+
+				transition 
+				group-even:right-[initial] 
+				group-even:-left-40 "
 			/>
 		</section>
 	);
