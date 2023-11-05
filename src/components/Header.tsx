453293f20e34f5e27dc855f1636895e878989917
Zach McLean <zmclean11@gmail.com>
2023-11-05 11:49:54 -0600

    initial re-design of header | plan to add a logo and resume button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,16 +14,20 @@ const Header = () => {
 			{/* This is the actual header */}
 			<motion.div
 				className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border 
-						border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg 
-						shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] 
-						sm:w-[36rem] sm:rounded-full
-						dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
-				initial={{ y: -100, x: "-50%", opacity: 0 }}
-				animate={{ y: 0, x: "-50%", opacity: 1 }}
+					border-white border-opacity-25 bg-white bg-opacity-25 shadow-lg 
+					shadow-black/[0.03] backdrop-blur-[0.2rem] 
+					dark:bg-gray-950 dark:border-black/10 dark:bg-opacity-25"
+				// className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border
+				// 		border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg
+				// 		shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem]
+				// 		sm:w-[36rem] sm:rounded-full
+				// 		dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+				initial={{ x: "-50%", opacity: 0 }}
+				animate={{ x: "-50%", opacity: 1 }}
 			></motion.div>
 			<nav
-				className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 
-						sm:top-[1.7rem] sm:h-[initial] sm:py-0"
+				className="flex fixed top-[0.8rem] left-1/2 h-12 -translate-x-1/2 py-2 
+						sm:h-[initial] sm:py-0"
 			>
 				<ul
 					className="flex w-[22rem] flex-wrap items-center justify-center 
@@ -40,7 +44,7 @@ const Header = () => {
 						>
 							<Link
 								className={clsx(
-									"flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+									"flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-350 dark:hover:text-gray-100",
 									{
 										"text-gray-950 dark:text-gray-200":
 											activeSection === link.name,
